8063d9afad851b389c8d0078223aff92a87fc332
kakadu <kakadu.hafanana@gmail.com>
2016-07-01 15:09:43 +0300

    Fix clang warning about comparing char with -1.
    We store in the char variable a number of found attributes and -1 when
    we found everything we need (see VT_TOKEN_ATTRS_MAX define). It seems that
    we store max 2 attributes at all.
    
    I changed type of this field from char to signed short.

--- src/drivers/char/terminal/vt.h
+++ src/drivers/char/terminal/vt.h
@@ -39,11 +39,11 @@ typedef enum vt_action vt_action_t;
 
 struct vt_token {
 	vt_action_t action;
-	char        ch;
-	char        attrs[VT_TOKEN_ATTRS_MAX];
-	char        attrs_len;
-	short      *params;
-	int         params_len;
+	char          ch;
+	char          attrs[VT_TOKEN_ATTRS_MAX];
+	signed short  attrs_len;
+	short        *params;
+	int           params_len;
 };
 
 #endif /* VT_H_ */
